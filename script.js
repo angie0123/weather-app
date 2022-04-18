9ae5384d20df771f2b516859109da725de109742
Angela Sun <angelasun1995@gmail.com>
2022-04-18 08:46:39 -0400

    Add fetching data function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('connected');
+async function getWeatherData() {
+  const response = await fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=835500ad3429a2468aae2adf3df00704'
+  );
+  console.log(response);
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+getWeatherData();
