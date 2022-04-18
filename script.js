3c2bc3815e0e47ff2930c0f2571c7934415a5112
Angela Sun <angelasun1995@gmail.com>
2022-04-18 09:02:32 -0400

    Add parsing data function

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ async function getWeatherData() {
   const response = await fetch(
     'https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=835500ad3429a2468aae2adf3df00704'
   );
-  console.log(response);
   const weatherData = await response.json();
-  console.log(weatherData);
+  return weatherData;
 }
 
-getWeatherData();
+const parseData = async () => {
+  const weatherData = await getWeatherData();
+  const parsed = {
+    city: weatherData.name,
+    temp: weatherData.main.temp,
+    feelsLike: weatherData.main.feels_like,
+    humidity: weatherData.main.humidity,
+    pressure: weatherData.main.pressure,
+    weather: weatherData.weather.main,
+  };
+  return parsed;
+};
+
+parseData();
