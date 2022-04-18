d847cfd6cf40bb76c1a38cc1cc5f2131c550af62
Angela Sun <angelasun1995@gmail.com>
2022-04-18 10:44:34 -0400

    Add units to data

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const data = (() => {
   async function getWeatherData(city) {
     const APIkey = '835500ad3429a2468aae2adf3df00704';
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`
     );
     const weatherData = await response.json();
     return weatherData;
@@ -16,7 +16,8 @@ const data = (() => {
       feelsLike: weatherData.main.feels_like,
       humidity: weatherData.main.humidity,
       pressure: weatherData.main.pressure,
-      weather: weatherData.weather.main,
+      weather: weatherData.weather[0].main,
+      date: new Date(weatherData.dt * 1000),
     };
     console.log(parsed);
     return parsed;
@@ -27,9 +28,14 @@ const data = (() => {
 const form = (() => {
   const input = document.querySelector('input');
   const submit = document.querySelector('button');
-  submit.addEventListener('click', async () => {
-    event.preventDefault();
-    const city = input.value;
-    await data.parseData(city);
-  });
+  const addHandler = () => {
+    submit.addEventListener('click', async () => {
+      event.preventDefault();
+      const city = input.value;
+      await data.parseData(city);
+    });
+  };
+  return { addHandler };
 })();
+
+form.addHandler();
